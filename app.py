32604576d931316d640f166208d0822376b393c6
Fred Kruger <chojnacki.radoslaw@tlen.pl>
2025-04-03 21:33:16 +0200

    start

---
+++ app.py
@@ -0,0 +1,14 @@
+
+class Fire:
+    # def __init__(self):
+    #     name = ''
+    
+    def init(self):
+        print('ECHO')
+
+
+
+
+####################################
+fire = Fire()
+fire.init()
